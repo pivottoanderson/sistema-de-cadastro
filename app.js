0314e92a93f8cd399db5dad915923f59cb1c6420
pivottoanderson <pivotto.anderson@gmail.com>
2023-06-18 20:11:49 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+console.log("o pauo fez esse commit sem voce ver na aula")
+console.log("enviando para o github")
